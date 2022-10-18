c8e9da3402b23002d9a64be565e884f8fe216104
daichi-sato-design <daichisato.design@gmail.com>
2022-10-18 23:11:46 +0900

    contextの使用とダークモードのトグルコンポーネントを使用

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,12 +1,12 @@
-import { useState } from 'react'
 import { Button } from 'antd'
+import ToggleTheme from '../components/ToggleTheme'
 
 const Home = () => {
-  const [name, setName] = useState('')
   return (
     <div>
       <h1>Home</h1>
       <Button type="primary">Clickme</Button>
+      <ToggleTheme />
     </div>
   )
 }
